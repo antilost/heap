07c17fb3b21071a42ee1a9d92b491d87dedc06ff
Ilya Antipin <antilost@yandex.ru>
2016-12-20 18:07:50 +0300

    examples go

---
+++ go/go_my_types.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a uint8 = 255; // uint8: 0 .. 255
+	fmt.Println("a =", a)
+	
+	var b uint16 = 65535; // uint8: 0 .. 65535
+	fmt.Println("b =", b)
+	
+	
+	
+	
+}
---
+++ go/hello_world.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// First GO-program
+// Hello world!
+
+func main() {
+	fmt.Println("Hello, world!")
+}
