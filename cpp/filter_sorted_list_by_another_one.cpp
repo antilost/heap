d7bfd7df72453583b9fd0c1432c5c3c06451d0a8
Ilya Antipin <antilost@yandex-team.ru>
2017-08-21 21:04:20 +0300

    solved

---
+++ cpp/filter_sorted_list_by_another_one.cpp
@@ -0,0 +1,59 @@
+/*
+ * Модификация задачи на слияние 2х списков.
+ * Есть 2 сортированных последовательности целых чисел.
+ * Хотим напечатать те элементы из первой последовательности, которых нет во второй последовательности.
+ * Элементы могут повторяться.
+ * Как водится, хотим это сделать без накопления в памяти (можно рассказать историю, что записи лежат в файликах до 100 ГБ на диске).
+ * 
+ * Test input:
+9
+3
+1 2 2 2 5 9 10 10 12
+-1 2 10
+ */
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+int main(void) {
+    int a_size, b_size;
+    cin >> a_size >> b_size;
+
+    vector<int> a(a_size);
+    vector<int> b(b_size);
+
+    while (a_size--) {
+        int v;
+        cin >> v;
+        a.push_back(v);
+    }
+    while (b_size--) {
+        int v;
+        cin >> v;
+        b.push_back(v);
+    }
+
+    int i = 0;
+    int j = 0;
+    while (i < a.size()) {
+        bool do_print = true;
+        if (j < b.size()) {
+            while (b[j] < a[i]) {
+                j++;
+            }
+            if (a[i] == b[j]) {
+                do_print = false;
+            }
+        }
+
+        if (do_print) {
+            cout << a[i] << " ";
+        }
+        i++;
+    }
+    cout << endl;
+
+    return 0;
+}
