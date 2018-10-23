8429faff38a5fbfffc86cf6a6582e5d6633b6f14
Ilya Antipin <antilost@yandex.ru>
2018-10-23 18:48:12 +0300

    http requests example

---
+++ python/test_http_requests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Script to provide basic example of HTTP POST-request sending
+
+import requests
+import json
+import getpass
+
+
+print('Get github user info')
+login = raw_input('login: ')
+password = getpass.getpass('password: ')
+result = requests.get('https://api.github.com/user', auth=(login, password))
+print("HTTP GET request code: " + str(result.status_code))
+print("Content-type: " + str(result.headers['content-type']))
+
+user_info = result.json()
+for k in sorted(user_info.keys(), key=lambda x: len(str(x))):
+    print('{0:25} {1}'.format( k, user_info[k]) )
