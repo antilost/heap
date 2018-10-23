06582e9c5c4cd59c8336c0aa0b3e644aebd70cab
Ilya Antipin <antilost@yandex.ru>
2018-10-23 17:02:59 +0300

    linked-list with reversion algorithm (linear complexity, constant memory)

---
+++ python/reverse_list.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# Classes to implement linked-list object
+# Reverse list algorithm
+
+class Node(object):
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+
+class List(object):
+    def __init__(self):
+        self.head = None
+    
+    def add(self, value):
+        n = Node(value, self.head)
+        self.head = n
+    
+    def values(self):
+        n = self.head
+        while (not (n is None)):
+            yield n.value
+            n = n.next
+
+    def reverse_list(self):
+        h = self.head
+        prev = None
+        while (not (h is None)):
+            t = h.next
+            h.next = prev
+            prev = h
+            h = t
+        self.head = prev
+            
+
+def main():
+    l = List()
+    l.add(1)
+    l.add(3)
+    l.add(8)
+    l.add(5)
+    l.add(4)
+    print([ v for v in l.values()])
+    l.reverse_list()
+    print([ v for v in l.values()])
+
+if __name__ == '__main__':
+    main()
