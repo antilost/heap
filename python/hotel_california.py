95660076e73bb4381546ae83b4a18f99e16ca41f
Ilya Antipin <antilost@yandex.ru>
2019-01-10 13:53:11 +0300

    add tests and combine two different realizations

--- python/hotel_california.py
+++ python/hotel_california.py
@@ -7,8 +7,28 @@
 
 # O(n log n) + O(n) time complexity: sort + one scanning
 # O(n) memory complexity
-
+def comp(x, y):
+    return x[1] - y[1] if x[0] == y[0] else x[0] - y[0]
 def get_max_guests_at_hotel(guests):
+    G = []
+    for g in guests:
+        G.append([g[0], 1])
+        G.append([g[1], 0])
+    G = sorted(G, cmp=comp)
+    j = 0
+    (now_guests_count, max_guests_count) = (0, 0)
+    for g in G:
+        if g[1] == 0:
+            now_guests_count -= 1
+        else:
+            now_guests_count += 1
+            max_guests_count = max([max_guests_count, now_guests_count])
+    return max_guests_count
+
+
+# O(n log n) + O(n) time complexity: sort + one scanning
+# O(n) memory complexity
+def get_max_guests_at_hotel_with_default_sort(guests):
     arrival = sorted([ g[0] for g in guests ])   # dates of arrival sorted
     departure = sorted([ g[1] for g in guests ]) # dates of departure sorted
     j = 0
@@ -21,12 +41,21 @@ def get_max_guests_at_hotel(guests):
         max_guests_count = max([max_guests_count, now_guests_count])
     return max_guests_count
 
-assert(get_max_guests_at_hotel([]) == 0)
-assert(get_max_guests_at_hotel([ [2,3], [1,2], [5,7] ]) == 1)
-assert(get_max_guests_at_hotel([ [1,3], [2,4], [3,7] ]) == 2)
-assert(get_max_guests_at_hotel([ [1,5], [1,5], [1,5], [2,5], [0,1] ]) == 4)
-assert(get_max_guests_at_hotel([ [0,2], [2,5], [0,4], [0,3], [1,5], [2,3], [0,5], [0,3], ]) == 7)
-assert(get_max_guests_at_hotel([ [0,5], [4,5], [3,5], [1,5], [4,5], [2,5], ]) == 6)
-assert(get_max_guests_at_hotel([ [0,1], [4,5], [1,2], ]) == 1)
-assert(get_max_guests_at_hotel([ [0,5], [2, 3], [1,2], [1,3], ]) == 3)
-assert(get_max_guests_at_hotel([ [7,8], [6,7], [4,5], [0,1], [1,4], [0,10] ]) == 2)
+
+
+test_cases = [
+    ([], 0),
+    ([ [2,3], [1,2], [5,7] ], 1),
+    ([ [1,3], [2,4], [3,7] ], 2),
+    ([ [1,5], [1,5], [1,5], [2,5], [0,1] ], 4),
+    ([ [0,2], [2,5], [0,4], [0,3], [1,5], [2,3], [0,5], [0,3], ], 7),
+    ([ [0,5], [4,5], [3,5], [1,5], [4,5], [2,5], ], 6),
+    ([ [0,1], [4,5], [1,2], ], 1),
+    ([ [0,5], [2, 3], [1,2], [1,3], ], 3),
+    ([ [7,8], [6,7], [4,5], [0,1], [1,4], [0,10] ], 2),
+]
+for input, result in test_cases:
+    assert(get_max_guests_at_hotel(input) == result)
+for input, result in test_cases:
+    assert(get_max_guests_at_hotel_with_default_sort(input) == result)
+    
