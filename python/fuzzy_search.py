3b164087eda0da45fdf2b6b8866d6b05c93d1b57
Ilya Antipin <antilost@yandex.ru>
2019-10-30 15:42:33 +0300

    fixed

--- python/fuzzy_search.py
+++ python/fuzzy_search.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python
 
-def fuzzysearch(string, pattern):
+def fuzzy_search_my(pattern, str):
     result = False
     i = 0
     j = 0
-    while ((i < len(string)) and (j < len(pattern))):
-        s = string[i]
+    while ((i < len(str)) and (j < len(pattern))):
+        s = str[i]
         p = pattern[j]
         if s == p:
-            i += 1
-        j += 1
+            j += 1
+        i += 1
  
-    if i >= len(string):
+    if j >= len(pattern):
         result = True
     return result
 
+def fuzzy_search_20191030(pattern, str):
+    i = 0
+    for s in str:
+        if pattern[i] == s:
+            i += 1
+            if i == len(pattern):
+                break
+    return i == len(pattern)
+
 def main():
-    pattern = "cartwheel"
-    for s, r in [
-        ('car', True),
-        ('cwhl', True),
-        ('cwheel', True),
-        ('cartwheel', True),
-        ('wee', True),
-        ('cwheeel', False),
-        ('lw', False)
-    ]:
-        result = fuzzysearch(s, pattern)
-        print('{} {} {}'.format(pattern, s, result))
-        assert(result == r)
+    fuzzy_search_implementations = [fuzzy_search_my, fuzzy_search_20191030]
+    for fuzzy_search in fuzzy_search_implementations:
+        str = "cartwheel"
+        for pattern, expected in [
+            ('car', True),
+            ('cwhl', True),
+            ('cwheel', True),
+            ('cartwheel', True),
+            ('wee', True),
+            ('cwheeel', False),
+            ('lw', False)
+        ]:
+            got = fuzzy_search(pattern, str)
+            print('{} {} {} {}'.format(fuzzy_search.__name__, pattern, str, got))
+            assert(got == expected)
 
 if __name__ == '__main__':
     main()
