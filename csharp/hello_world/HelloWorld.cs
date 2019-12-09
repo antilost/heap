c164056613c0e067d54a9d3dcc92c6b8f0be4df3
Ilya Antipin <antilost@yandex.ru>
2019-12-09 12:39:31 +0300

    save

---
+++ csharp/hello_world/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace System
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
