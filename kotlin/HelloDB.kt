21b1fb71e28559d7a44fc927d097a3b1297a22b7
Ilia Antipin <anti@joom.com>
2022-07-15 14:25:07 +0300

    test kotlin sctipt

---
+++ kotlin/HelloDB.kt
@@ -0,0 +1,13 @@
+/*
+anti@anti-osx code % ls
+HelloDB.kt
+anti@anti-osx code % kotlinc-jvm HelloDB.kt        
+anti@anti-osx code % ls
+HelloDB.kt	HelloDBKt.class	META-INF
+anti@anti-osx code % kotlin HelloDBKt              
+Hello, db!
+*/
+
+fun main() {
+    println("Hello, db!")
+}
